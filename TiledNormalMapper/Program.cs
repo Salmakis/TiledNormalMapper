5780cae1344817d82f8f25e30cbfeae0c15755e6
Salmakis <zalmakiz@googlemail.com>
2020-10-16 00:13:48 +0200

    removed craig´s utilities and switched to a simple implementation.

--- TiledNormalMapper/Program.cs
+++ TiledNormalMapper/Program.cs
@@ -2,7 +2,6 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.IO;
-using Utilities.Media;
 
 class Program
 {
@@ -33,8 +32,6 @@ class Program
 		Bitmap output = new Bitmap(input.Height, input.Height);
 		Graphics outputCanvasG = Graphics.FromImage(output);
 		outputCanvasG.InterpolationMode = InterpolationMode.NearestNeighbor;
-		
-		
 		
 		for (int tileX = 0; tileX < input.Width; tileX += tileW)
 		{
@@ -79,15 +76,9 @@ class Program
 					tileY + tileH - 1, 0.1f, 0.1f,
 					GraphicsUnit.Pixel);
 
-				//now convert this tile
-				// convertCanvas = new NormalMap().Create(convertCanvas);
 				convertCanvasG.Dispose();
-				SwiftBitmap swiftBitmap = new SwiftBitmap(convertCanvas);
-				swiftBitmap.Lock();
-				swiftBitmap = swiftBitmap.NormalMap();
-				Bitmap outbmp = swiftBitmap.InternalBitmap;
-				swiftBitmap.Unlock();
-				//blit the converted result to the output file
+				Bitmap outbmp = CraeteNormalMap(convertCanvas);// swiftBitmap.InternalBitmap;
+
 				outputCanvasG.DrawImage(outbmp, new Rectangle(tileX, tileY, tileW, tileH), new Rectangle(mW, mW, tileW, tileH),GraphicsUnit.Pixel);
 			}
 		}
@@ -97,5 +88,40 @@ class Program
 		string outputFile = args[0].Replace(ext, $"_normal{ext}");
 		output.Save(outputFile);
 		Console.WriteLine($"saved to:{outputFile}");
+	}
+	
+	/**
+	 * translated from unity stuff to normal bitmap from:
+	 * https://gamedev.stackexchange.com/questions/106703/create-a-normal-map-using-a-script-unity
+	 */
+	
+	private static Bitmap CraeteNormalMap(Bitmap source,float strength = 1f) 
+	{
+		strength=Math.Clamp(strength,0.0F,1.0F);
+
+		Bitmap normalTexture;
+		float xLeft;
+		float xRight;
+		float yUp;
+		float yDown;
+		float yDelta;
+		float xDelta;
+
+		normalTexture = new Bitmap (source.Width, source.Height);
+
+		for (int y=1; y+1<normalTexture.Height; y++) 
+		{
+			for (int x=1; x+1<normalTexture.Width; x++) 
+			{
+				xLeft = (source.GetPixel(x-1,y).R/255f)*strength;
+				xRight = (source.GetPixel(x+1,y).R/255f)*strength;
+				yUp = (source.GetPixel(x,y-1).R/255f)*strength;
+				yDown = (source.GetPixel(x,y+1).R/255f)*strength;
+				xDelta = ((xLeft-xRight)+1)*0.5f;
+				yDelta = ((yDown-yUp)+1)*0.5f;
+				normalTexture.SetPixel(x,y,Color.FromArgb(255,  (int)(xDelta * 255f),(int)(yDelta * 255), 255));
+			}
+		}
+		return normalTexture;
 	}
 }
